f2b172b1b65f73eda609d1a65c94b8d610ffbcb0
Camille Moatti <camille.moatti@gmail.com>
2020-11-24 13:08:08 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+app = FastAPI()
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, skip: int = 0, limit: int = 10):
+    return {"item_id": item_id, "skip": skip, "limit": limit}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
