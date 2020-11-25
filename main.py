2e89a312792e59b4fb15c27c4a5b1b4d72536bb4
Camille Moatti <camille.moatti@gmail.com>
2020-11-25 14:21:59 +0100

    added id and range

--- main.py
+++ main.py
@@ -24,3 +24,21 @@ app = FastAPI()
 def read_customers(skip: int = 0, limit: int = 100,
                    db: Session = Depends(get_db)):
     return db.query(models.Customers).offset(skip).limit(limit).all()
+
+
+@app.get("/customers/{id}", response_model=schemas.Customers,
+         status_code=status.HTTP_200_OK)
+def read_customer(id: int, db: Session = Depends(get_db)):
+    return db.query(models.Customers). \
+        filter(models.Customers.CustomerId == id).scalar()
+
+
+@app.get("/customers/from/{id_start}/to/{id_end}",
+         response_model=List[schemas.Customers],
+         status_code=status.HTTP_200_OK)
+def read_customers_range(id_start: int, id_end: int,
+                         db: Session = Depends(get_db)):
+    if id_start > id_end:
+        raise
+    return db.query(models.Customers). \
+        filter(models.Customers.CustomerId.between(id_start, id_end)).all()
